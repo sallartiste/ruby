890da3d863177ff6b8cf42b207d51644495be641
sallartiste <sallartiste@gmail.com>
2018-04-09 11:29:50 +0200

    update

--- eath-2/tableau/carnet.rb
+++ eath-2/tableau/carnet.rb
@@ -3,16 +3,7 @@ adresse_de_nicolas = {
     "rue"          => "Rue du port, 32" ,
     "code postal"  => "56000" ,
     "ville"        => "Vannes" ,
-    "pays"         => "France" 
-    }
-
-#Nicolas
-nicolas = {
-    "prénom"         => "Nicolas",
-    "nom de famille" => "Rocher",
-    "téléphone"      => "(+33) 03 32 25 45",
-    "email"          => "nico@gmail.com",
-    "adresse  "      => adresse_de_nicolas
+    "pays"         => "France"
     }
 
 #adresse de François
@@ -20,16 +11,7 @@ adresse_de_francois = {
     "rue"          => "Rue de la tranchée, 14" ,
     "code postal"  => "1000" ,
     "ville"        => "Bruxelles" ,
-    "pays"         => "Belgique" 
-    }
-
-#François
-francois = {
-    "prénom"         => "François",
-    "nom de famille" => "Willemart",
-    "téléphone"      => "(+32) 02 32 25 45",
-    "email"          => "will_francois@gmail.com",
-    "adresse  "      => adresse_de_francois
+    "pays"         => "Belgique"
     }
 
 #adresse de Marina
@@ -37,16 +19,7 @@ adresse_de_marina = {
     "rue"          => "Strada di l'amore, 61" ,
     "code postal"  => "50100" ,
     "ville"        => "Rome" ,
-    "pays"         => "Italie" 
-    }
-
-#Marina
-marina = {
-    "prénom"         => "Marine",
-    "nom de famille" => "Mantini",
-    "téléphone"      => "(+39) 055 32 45",
-    "email"          => "marina@gmail.com",
-    "adresse  "      => adresse_de_marina
+    "pays"         => "Italie"
     }
 
 #adresse de Eric
@@ -54,39 +27,65 @@ adresse_de_eric = {
     "rue"          => "Rue de Ribaucourt, 161" ,
     "code postal"  => "1080" ,
     "ville"        => "Bruxelles" ,
-    "pays"         => "Belgique" 
+    "pays"         => "Belgique"
     }
 
-#Eric
-eric = {
-    "prénom"         => "Eric",
-    "nom de famille" => "Salla",
-    "téléphone"      => "(+32) 02 05 32 45",
-    "email"          => "sallartiste@gmail.com",
-    "adresse  "      => adresse_de_eric
-    }
+    # Nicolas
+  nicolas = {
+      "prénom"         => "Nicolas",
+      "nom de famille" => "Rocher",
+      "téléphone"      => "(+33) 02 93 45 49 19",
+      "email"          => "nico@becode.org",
+      "adresse"        => adresse_de_nicolas
+  }
+
+  # François
+  francois = {
+      "prénom"         => "François",
+      "nom de famille" => "Willemart",
+      "téléphone"      => "(+32) 02 679 24 81",
+      "email"          => "francois@becode.org",
+      "adresse"        => adresse_de_francois
+  }
+
+  # Marina
+  marina = {
+      "prénom"         => "Marina",
+      "nom de famille" => "Nantini",
+      "téléphone"      => "(+39) 055 681 32 11",
+      "email"          => "marina@becode.org",
+      "adresse"        => adresse_de_marina
+  }
+
+  #Eric
+  eric = {
+      "prénom"         => "Eric",
+      "nom de famille" => "Salla",
+      "téléphone"      => "(+39) 055 681 32 11",
+      "email"          => "sallartiste@becode.org",
+      "adresse"        => adresse_de_eric
+  }
+
+
 
 carnet = [nicolas, francois, marina, eric]
 
-puts 
-carnet.each do |tab|
-    puts tab['adresse']
-end
+puts
 
-=begin
+
+
 carnet.each do |personne|
     #Nom et Email
-    nom = personne['nom de famille']
-    prenom = personne ['prénom']
-    email = personne ['email']
-    
-    puts "#{prenom} #{nom}\n     #{email}"
-    puts
-    
+    nom = personne["nom de famille"]
+    prenom = personne ["prénom"]
+    email = personne ["email"]
+    tel = personne ["téléphone"]
+
+    puts "#{prenom} #{nom}\n     #{email}\n     #{tel}"
+
     #adresse
     rue = personne["adresse"]["rue"]
-    puts rue
-    
-end
-=end
+    puts "      " + rue
 
+
+end
